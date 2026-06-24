a5f4edf9556987fb334338a0a4f4891846595971
hans1801 <hansacha.1801@gmail.com>
2026-06-24 11:28:16 -0500

    add docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 5678,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: process.env.VITE_BACKEND_HOST || 'http://localhost:8000',
         changeOrigin: true,
       },
     },
